82407c73444024a41702cd12834e5dc3f83232ca
dev-adeshina <shina.oladoja@outlook.com>
2026-08-04 17:31:44 +0100

    vercel setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->trustProxies(
+            '*',
+            Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO
+        );
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
