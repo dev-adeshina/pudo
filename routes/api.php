1461c5a9661d63991e8494b568db00121b512020
dev-adeshina <shina.oladoja@outlook.com>
2026-08-08 10:35:19 +0100

    corrected typo in route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->User();
+    return $request->user();
 })->middleware('auth:sanctum');
 
 
