31e61ed1e6c698d77c39358e328305f42cf8c310
dev-adeshina <shina.oladoja@outlook.com>
2026-08-08 10:32:56 +0100

    corrected token mismatched issue

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->UserRes();
+    return $request->User();
 })->middleware('auth:sanctum');
 
 
