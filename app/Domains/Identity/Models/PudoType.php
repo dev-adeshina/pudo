e1f12f67f1ea684764e78aeb73a20f63a19e79e5
dev-adeshina <shina.oladoja@outlook.com>
2026-08-16 20:04:23 +0100

    incorporating vendor listing

---
+++ app/Domains/Identity/Models/PudoType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domains\Identity\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PudoType extends Model
+{
+    //
+}
--- routes/pudo.php
+++ routes/pudo.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Domains\Identity\Models\PudoType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoorWay\Pudo\RegisterController;
@@ -7,6 +8,7 @@ use App\Http\Controllers\DoorWay\Pudo\RegisterController;
 
 Route::prefix('pudo')->group(function () {
     Route::post('/register', [RegisterController::class, 'register'])->defaults('access_point_id', 3);
+    Route::get('/type', function() {return PudoType::all();})->middleware('auth:sanctum');
    require __DIR__ . "/pudo/vendor.php";
 });
 
